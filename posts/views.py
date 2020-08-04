5fdfa3d287cff6467a9acdb589edf22a355e939f
Максим Секачёв <cement-fan@ya.ru>
2020-08-04 12:23:54 +0300

    after hw02 and add test ordering)

---
+++ posts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+
+from .models import Post, Group, User
+
+import datetime
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
+
+def test(request):
+    author = User.objects.get(username="leo")
+    keyword = "утро"
+    start_date = datetime.date(1854, 7, 7)
+    end_date = datetime.date(1854, 7, 21)
+    posts = Post.objects.filter(
+        text__contains=keyword
+        ).filter(
+        pub_date__range=(start_date, end_date)
+        ).filter(
+            author=author
+        )
+
+    return render(request, "index.html", {"posts": posts})
